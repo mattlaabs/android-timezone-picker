ac7960e110201f3b3583b27e6e673f4ffd15eb92
Matthias <matthias@tapirapps.de>
2018-09-06 15:35:26 +0200

    update to supportlib

--- android-timezone-picker/src/com/android/timezonepicker/TimeZonePickerDialog.java
+++ android-timezone-picker/src/com/android/timezonepicker/TimeZonePickerDialog.java
@@ -17,8 +17,8 @@
 package com.android.timezonepicker;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
